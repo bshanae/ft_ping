e9f86fc6f1240621657db01de51d4679445d4210
v.belchenko <belchenkovova@gmail.com>
2022-08-14 10:28:09 +0300

    Removed container configuration, because it is easier to use a virtual machine.

--- env/run.sh
+++
@@ -1,7 +0,0 @@
-docker run -it                   \
-		   --rm                  \
-		   -e COLUMNS= -e LINES= \
-		   -v $PWD:/ft_ping      \
-		   --network host        \
-		   ft_ping			     \
-		   bash
